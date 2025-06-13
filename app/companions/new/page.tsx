0e9a3598b69960e5c87c3bc6be8bc36efc5b6873
Dhuvarakesh17 <dhuvarakeshs92@gmail.com>
2025-06-14 00:40:35 +0530

    Authenticate and database connect

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import CompanionForm from "@/components/CompanionForm";
-const NewCompanion=()=> {
+// @ts-ignore
+import {auth} from "@clerk/nextjs/server";
+// @ts-ignore
+
+import {redirect} from "next/navigation";
+
+const NewCompanion=async ()=> {
+    const {userId}=await auth();
+    if(!userId) redirect('/sign-in')
+
+
+
     return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
         <article className="w-full gap-4 flex flex-col">
