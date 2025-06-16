973f5daece29510110a32dad6e936f8217d2b9a4
Dhuvarakesh17 <dhuvarakeshs92@gmail.com>
2025-06-16 15:08:03 +0530

    Integrate Vapi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+export const nextConfig: NextConfig = {
+  images:{
+      remotePatterns:[
+          {hostname:'img.clerk.com'}
+      ]
+  }
 };
 
+// @ts-ignore
 export default nextConfig;
